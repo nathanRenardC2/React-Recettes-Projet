c77ce20f73d9942af8c1d5507bf65c88005b376a
Nathan Renard <nathan.renard@etu.univ-grenoble-alpes.fr>
2022-11-29 20:08:51 +0100

    fixed dark mode

--- src/components/Recettes/Recette.jsx
+++ src/components/Recettes/Recette.jsx
@@ -56,7 +56,7 @@ export default function Recette({ meal, id }) {
         </div>
       </NavLink>
       <div>
-        <svg onClick={saveRecette} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-7 h-7 self-end mr-5 mb-3 hover:cursor-pointer hover:text-red-500 dark:text-white transition ${isSave ? "text-red-500" : ""}`}>
+        <svg onClick={saveRecette} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-7 h-7 self-end mr-5 mb-3 hover:cursor-pointer hover:text-red-500 dark:text-white transition ${isSave ? "text-red-500 dark:text-red-500" : ""}`}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
         </svg>
       </div>
